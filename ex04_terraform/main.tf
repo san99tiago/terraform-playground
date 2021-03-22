b41a6ad2978100762397bf79f96c3cfde9acecab
Santiago Garcia <santiago.garcia1999@hotmail.com>
2021-03-22 16:14:58 -0500

    Updated ex04 with extra AWS services vpc, subnets, ig

--- ex04_terraform/main.tf
+++ ex04_terraform/main.tf
@@ -1,38 +1,97 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
-resource "aws_instance" "my-cool-terraform-instance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  tags            = var.tags
-  security_groups = [aws_security_group.tcp_ssh_multiple_connections.name]
+resource "aws_vpc" "my-terraform-vpc" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = var.vpc_instance_tenancy
+  tags             = var.vpc_tags
 }
 
+resource "aws_internet_gateway" "my-terraform-internet-gw" {
+  vpc_id = aws_vpc.my-terraform-vpc.id
+  tags   = var.gw_tags
+}
 
-resource "aws_security_group" "tcp_ssh_multiple_connections" {
+resource "aws_route_table" "my-terraform-rt" {
+  vpc_id = aws_vpc.my-terraform-vpc.id
+  tags   = var.rt_tags
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-terraform-internet-gw.id
+  }
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.my-terraform-internet-gw.id
+  }
+}
+
+resource "aws_subnet" "my-terraform-subnet-A" {
+  vpc_id            = aws_vpc.my-terraform-vpc.id
+  cidr_block        = var.vpc_subnet_cidr_block
+  availability_zone = var.aws_availability_zone
+  tags              = var.vpc_subnet_tags
+}
+
+resource "aws_route_table_association" "my-terraform-rt-association" {
+  subnet_id      = aws_subnet.my-terraform-subnet-A.id
+  route_table_id = aws_route_table.my-terraform-rt.id
+}
+
+resource "aws_security_group" "my-terraform-security-group" {
   name        = var.sg_name
-  description = "Allow TCP and SSH connections."
-
+  description = "Allow custom TCP and SSH connections."
+  vpc_id      = aws_vpc.my-terraform-vpc.id
   dynamic "ingress" {
-    for_each = var.ingress_rules
+    for_each = var.sg_ingress_rules
     content {
-      description = "SSH ingress rule"
+      description = "SG ingress rule"
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
       cidr_blocks = ingress.value.cidr_blocks
     }
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "egress" {
+    for_each = var.sg_egress_rules
+    content {
+      description = "SG egress rule"
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
+  tags = var.sg_tags
+}
 
-  tags = {
-    Name = "allow_tcp_and_ssh"
+resource "aws_network_interface" "my-terraform-network-interface" {
+  subnet_id       = aws_subnet.my-terraform-subnet-A.id
+  private_ips     = [var.network_interface_private_ips]
+  security_groups = [aws_security_group.my-terraform-security-group.id]
+}
+
+resource "aws_eip" "my-terraform-eip" {
+  vpc                       = true
+  network_interface         = aws_network_interface.my-terraform-network-interface.id
+  associate_with_private_ip = var.network_interface_private_ips
+  depends_on                = [aws_internet_gateway.my-terraform-internet-gw]
+}
+
+resource "aws_instance" "my-terraform-instance" {
+  ami               = var.instance_ami_id
+  instance_type     = var.instance_type
+  availability_zone = var.aws_availability_zone
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.my-terraform-network-interface.id
   }
+  user_data = <<-EOF
+      #!/bin/bash
+      sudo apt update -y
+      sudo apt install apache2 -y
+      sudo systemctl start apache2
+      sudo bash -c 'echo my first cool web server from IaC > /var/www/html/index.html'
+      EOF
+  tags      = var.instance_tags
 }
--- ex04_terraform/variables.tf
+++ ex04_terraform/variables.tf
@@ -1,4 +1,74 @@
-variable "ami_id" {
+variable "aws_region" {
+  type        = string
+  description = "Region for thw AWS deployment."
+}
+
+variable "aws_availability_zone" {
+  type        = string
+  description = "Availability Zone within selected Region."
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Cidr_block for the VPC."
+}
+
+variable "vpc_instance_tenancy" {
+  type        = string
+  description = "Instance tenancy for the VPC."
+}
+
+variable "vpc_tags" {
+  type        = map(string)
+  description = "Tags for the VPC."
+}
+
+variable "gw_tags" {
+  type        = map(string)
+  description = "Tags for the GateWay."
+}
+
+variable "rt_tags" {
+  type        = map(string)
+  description = "Tags for the Route Table."
+}
+
+variable "vpc_subnet_cidr_block" {
+  type        = string
+  description = "Cidr_block for the Subnet for the VPC."
+}
+
+variable "vpc_subnet_tags" {
+  type        = map(string)
+  description = "Tags of the Subnet for the VPC."
+}
+
+variable "sg_name" {
+  type        = string
+  description = "Name for the security group."
+}
+
+variable "sg_tags" {
+  type        = map(string)
+  description = "Tags for the security group."
+}
+
+variable "sg_ingress_rules" {
+  type        = any
+  description = "Ingress rules for the security group."
+}
+
+variable "sg_egress_rules" {
+  type        = any
+  description = "Egress rules for the security group."
+}
+
+variable "network_interface_private_ips" {
+  type        = any
+  description = "Private IPs for the Network Interface."
+}
+
+variable "instance_ami_id" {
   type        = string
   description = "The id of the machine image (AMI) to use for the server."
 }
@@ -9,17 +79,7 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "tags" {
+variable "instance_tags" {
   type        = map(string)
   description = "Tags for the EC2 instance."
-}
-
-variable "sg_name" {
-  type        = string
-  description = "Name for the security group."
-}
-
-variable "ingress_rules" {
-  type        = any
-  description = "Rules for the security group."
 }
