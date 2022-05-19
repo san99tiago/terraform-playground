257710d5c04bb90f61d019ccecdf157fff4f138e
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-05-18 21:48:15 -0500

    Add ex07_loops_with_lists for count and for_each

---
+++ ex07_loops_with_lists/count/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# LIST OF USERS
+variable "user_names" {
+  description = "IAM usernames"
+  type        = list(string)
+  default     = ["santi", "moni", "elkin"]
+}
+
+# COUNT USAGE
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+
+# FOR USAGE
+output "print_the_names" {
+  value = [for user_name in var.user_names : "The specific user_name is: ${user_name} and its index is ${index(var.user_names, "${user_name}")}"]
+}
